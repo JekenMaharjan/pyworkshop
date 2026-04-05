1f931fb965a913be636407e999af007258e1c90e
JekenMaharjan <maharjanjeken@gmail.com>
2026-04-05 09:29:12 +0545

    Complete random number generator app

---
+++ 14_random_number_generator.py
@@ -0,0 +1,12 @@
+# ===============================================================
+# Random Number Generator
+# ===============================================================
+
+# import random to get randomized
+import random
+
+# randomized number in between range 1 to 100
+random_number = random.randint(1, 100)
+
+# print random number
+print(random_number)
